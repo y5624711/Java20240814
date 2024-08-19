863ce4e7ad74f027d2ceae1337db34f75c982b9c
Yun <y5624711@gmail.com>
2024-08-19 11:25:40 +0900

    TextBlockExample

---
+++ src/ch02/lecture/sec06/TextBlockExample.java
@@ -0,0 +1,23 @@
+package ch02.lecture.sec06;
+
+public class TextBlockExample {
+    public static void main(String[] args) {
+        String str1 = "" +
+                "{\n" +
+                "\t\"id\":\"winter\",\n" +
+                "\t\"name\":\"눈송이\",\n" +
+                "}";
+
+        String str2 = """
+                {
+                  "id":"winter"
+                  "name":"눈송이"
+                }
+                """;
+
+        System.out.println(str1);
+        System.out.println("------------------------");
+        System.out.println(str2);
+        System.out.println("------------------------");
+    }
+}

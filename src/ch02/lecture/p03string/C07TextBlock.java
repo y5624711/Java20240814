aa9a627bf2c82d2642c085f9e5498f50efb5db6a
Yun <y5624711@gmail.com>
2024-08-19 11:25:34 +0900

    TextBlock

---
+++ src/ch02/lecture/p03string/C07TextBlock.java
@@ -0,0 +1,36 @@
+package ch02.lecture.p03string;
+
+public class C07TextBlock {
+    public static void main(String[] args) {
+        String a = "동해물과 백두산이 \n 마르고 닳도록\n";
+        System.out.println("a = " + a);
+
+        String b = "동해물과 백두산이\n" +
+                "마르고 닳도록\n";
+        System.out.println("b = " + b);
+
+        //text block
+        //작성한 대로 문자열 완성
+        //첫""" 이후 엔터
+        // 가장 왼쪽 들여쓰기 기준으로 들여쓰기 결정
+        String c = """
+                동해물과 백두산이
+                마르고 닳도록
+                """;
+        System.out.println("c = " + c);
+
+        // "
+        String html1 = "<html>\n" + "<head>\n" + "<body>\n" + "</body>\n" + "</head>\n" + "</html>";
+        System.out.println(html1);
+
+        String html2 = """
+                <html>
+                  <head>
+                  <body>
+                  </body>
+                  </head>
+                </html>
+                """;
+
+    }
+}

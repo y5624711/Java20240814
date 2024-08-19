ed5147f49981956e57309c271e7daaf833517368
Yun <y5624711@gmail.com>
2024-08-19 17:02:53 +0900

    Conversion

---
+++ src/ch02/lecture/p04conversion/C09Var.java
@@ -0,0 +1,13 @@
+package ch02.lecture.p04conversion;
+
+public class C09Var {
+    public static void main(String[] args) {
+        //카워드는 변수명으로 사용 불가
+        int i =3;
+        
+        int var = 3; //var은 변수명(심볼)로 사용가능
+        
+        var j =5;
+        
+    }
+}

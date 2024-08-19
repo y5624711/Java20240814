57e5becb5e1a4481eee21eb4ca45564ee76df926
Yun <y5624711@gmail.com>
2024-08-19 12:37:04 +0900

    Conversion

---
+++ src/ch02/lecture/p04conversion/C04TypeConversion.java
@@ -0,0 +1,19 @@
+package ch02.lecture.p04conversion;
+
+public class C04TypeConversion {
+    public static void main(String[] args) {
+        // 자동형변환 : 문자형(char) -> 정수형 (int, long)
+        // 강제형변환 : 정수형(int , long) -> 문자형(char)
+
+        char a ='한';
+        int b = a;
+        System.out.println("a = " + a);
+        System.out.println("b = "  +b);
+
+        long c =b;
+        System.out.println("c = " +c);
+
+        a = (char)b;
+        System.out.println("a = " +a);
+    }
+}

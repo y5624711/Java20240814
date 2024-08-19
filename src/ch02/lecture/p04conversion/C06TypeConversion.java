8cfc45c98ec233c55a02f7b3d82b67449e0daca3
Yun <y5624711@gmail.com>
2024-08-19 14:22:31 +0900

    Conversion

---
+++ src/ch02/lecture/p04conversion/C06TypeConversion.java
@@ -0,0 +1,18 @@
+package ch02.lecture.p04conversion;
+
+public class C06TypeConversion {
+    public static void main(String[] args) {
+
+        //byte short int 끼리의 연산의 결과는 int
+        byte a =3;
+        byte b =5;
+        byte c =(byte)( a + b);
+
+        //정수 끼리의 연산 결과는 정수
+        int d = 5;
+        int e =3;
+        int f= d/e;
+
+
+    }
+}
---
+++ src/ch02/lecture/p04conversion/C07TypeConversion.java
@@ -0,0 +1,14 @@
+package ch02.lecture.p04conversion;
+
+public class C07TypeConversion {
+    public static void main(String[] args) {
+        //강제형변환(casting)은 기본타입간, 또는 참조 타입간에만 사용가능
+        long a =3;
+        int b= (int)a; // 기본타입끼리의 캐스팅 가능
+
+
+//        String c =(String)
+        String d = "3";
+//        int e = (int)d ;//기본타입과 참조타입간의 형변환 x
+    }
+}

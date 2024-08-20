ff0787e0b5d711e46720e7ab3a91b08a01c63dfa
Yun <y5624711@gmail.com>
2024-08-20 14:55:13 +0900

    priority

---
+++ src/ch03/lecture/p05priority/C01Priority.java
@@ -0,0 +1,26 @@
+package ch03.lecture.p05priority;
+
+public class C01Priority {
+    public static void main(String[] args) {
+        //연산자 우선순위
+
+        int a = 3 + 5 * 2;
+
+        System.out.println("a = " + a);
+        
+        //()로 먼저 연산되는 부분을 감싸기
+        //shift + 9 선택부분 괄호
+        int b = 3 + (5 * 2);
+        System.out.println("b = "+ b);
+
+        int x = 10;
+        int y = 5;
+
+        System.out.println(x % 3 == 2 || y % 2 !=1);
+        System.out.println(x % 3 == 2 || y % 2 !=1);
+        System.out.println((x % 3) == 2 || (y % 2) !=1);
+
+        
+
+    }
+}

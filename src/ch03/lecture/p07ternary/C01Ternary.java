1c4aba81435996f8ec84747c8c7960350e374178
Yun <y5624711@gmail.com>
2024-08-20 16:41:46 +0900

    ternary

---
+++ src/ch03/lecture/p07ternary/C01Ternary.java
@@ -0,0 +1,20 @@
+package ch03.lecture.p07ternary;
+
+public class C01Ternary {
+    public static void main(String[] args) {
+        //삼항 연산자(조건연산자)
+        // a ? b : c
+        // 연산결과는 a가 true 이면 b
+        // false 이면 c
+
+        int a = true ? 5 : 3;
+        System.out.println("a = " + a);
+
+        int b = false ? 5 : 3;
+        System.out.println("b = " + b);
+
+
+        //이항 연산자 a+b
+        //일한 연산자 a++
+    }
+}

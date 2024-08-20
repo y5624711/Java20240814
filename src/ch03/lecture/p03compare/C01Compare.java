00216494524eada43e7077530b99ad1efcf6eeaa
Yun <y5624711@gmail.com>
2024-08-20 12:46:08 +0900

    comapare

---
+++ src/ch03/lecture/p03compare/C01Compare.java
@@ -0,0 +1,19 @@
+package ch03.lecture.p03compare;
+
+public class C01Compare {
+    public static void main(String[] args) {
+        //비교연산자
+        //같다, 같지않다, 크다, 크거나 같다, 작다, 같거다 작다.
+        //== , !=, >, >=, <, <=
+
+        //연산결과는 boolean 이다
+        int a =3;
+        int b= 5;
+        System.out.println(a==b);
+        System.out.println(a!=b);
+        System.out.println(a>b);
+        System.out.println(a>=b);
+        System.out.println(a<b);
+        System.out.println(a<=b);
+    }
+}
---
+++ src/ch03/lecture/p03compare/C03String.java
@@ -0,0 +1,25 @@
+package ch03.lecture.p03compare;
+
+public class C03String {
+    public static void main(String[] args) {
+        String a = "abc";
+        String b = "def";
+
+        //크다, 작다 할 수 없음
+//        System.out.println(a>b);
+
+        //같다, 작다 할 수 있음
+        //하지만 하면 안됨!!!!!!!
+        System.out.println(a == b);
+
+        String c = "html";
+        String d = "ht";
+        String e = "ml";
+        String f = d+e;
+        System.out.println("c = " + c);
+        System.out.println("f = " + f);
+
+        System.out.println(c == f);
+        System.out.println(c.equals(f));
+    }
+}
